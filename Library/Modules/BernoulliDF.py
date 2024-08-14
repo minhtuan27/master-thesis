b71abfa817d8c96f907885af13525aa795598760
visnic <minhtuan270820000@gmail.com>
2024-08-14 15:48:56 +0100

    Bernoulli DF on toy dataset

--- Library/Modules/BernoulliDF.py
+++ Library/Modules/BernoulliDF.py
@@ -13,17 +13,17 @@ class BernoulliDF(tf.Module):
         X: tf.Tensor of shape (r, d)
     """
 
-    def __init__(self, m, n, r, gamma_X, gamma_C, sigma_C, name=None):
+    def __init__(self, m, n, r, gamma_X, sigma_X, gamma_C, sigma_C, name=None):
         super().__init__(name=name)
         
-        self.C = tf.Variable(tf.random.truncated_normal([m, r], mean=0.0, stddev=1.0, dtype=tf.float64))
-        self.X = tf.Variable(tf.random.truncated_normal([r, n], mean=0.0, stddev=1.0, dtype=tf.float64))
+        self.C = tf.Variable(tf.random.truncated_normal([m, r], mean=0.0, stddev=sigma_C, dtype=tf.float64))
+        self.X = tf.Variable(tf.random.truncated_normal([r, n], mean=0.0, stddev=sigma_X, dtype=tf.float64))
         
         self.gamma_X = tf.constant(gamma_X, dtype=tf.float64)
+        self.sigma_X = tf.constant(sigma_X ** 2, dtype=tf.float64)
         self.gamma_C = tf.constant(gamma_C, dtype=tf.float64)
         self.sigma_C = tf.constant(sigma_C ** 2, dtype=tf.float64)
 
-        self.zero = tf.constant(0.0, dtype=tf.float64)
         self.one = tf.constant(1.0, dtype=tf.float64)
         self.two = tf.constant(2.0, dtype=tf.float64)
         self.four = tf.constant(4.0, dtype=tf.float64)
@@ -45,39 +45,42 @@ class BernoulliDF(tf.Module):
             )
         )
     
-    @tf.function
-    def log_pi(self, Y, C, X):
-        return tf.subtract(
-            tf.reduce_sum(
-                tf.add(
-                    tf.multiply(Y, tf.math.log(tf.sigmoid(tf.matmul(C, X)))),
-                    tf.multiply(tf.subtract(self.one, Y), tf.math.log(tf.subtract(self.one, tf.sigmoid(tf.matmul(C, X)))))
-                )
-            ),
-            tf.divide(tf.square(tf.norm(C)), self.two * self.sigma_C)
-        )
+    # @tf.function
+    # def log_pi(self, Y, C, X):
+    #     return tf.subtract(
+    #         tf.reduce_sum(
+    #             tf.add(
+    #                 tf.multiply(Y, tf.math.log_sigmoid(tf.matmul(C, X))),
+    #                 tf.multiply(tf.subtract(self.one, Y), tf.math.log_sigmoid(tf.negative(tf.matmul(C, X))))
+    #             )
+    #         ),
+    #         tf.divide(tf.square(tf.norm(C)), tf.multiply(self.two, self.sigma_C))
+    #     )
     
-    @tf.function
-    def log_q(self, Y, X, C_tilde, C):
-        return tf.subtract(
-            self.zero,
-            tf.divide(
-                tf.square(tf.norm(tf.subtract(C_tilde, tf.add(C, self.gradient_log_pi(Y, C, X))))),
-                self.four * self.gamma_C
-            )
-        )
+    # @tf.function
+    # def log_q(self, Y, X, C_tilde, C):
+    #     return tf.negative(
+    #         tf.divide(
+    #             tf.square(tf.norm(tf.subtract(C_tilde, tf.add(C, self.gradient_log_pi(Y, C, X))))),
+    #             tf.multiply(self.four, self.gamma_C)
+    #         )
+    #     )
     
-    @tf.function
-    def metropolis_hastings(self, Y, X, C_tilde, C):
-        return tf.minimum(
-            self.one,
-            tf.math.exp(
-                tf.subtract(
-                    tf.add(self.log_pi(Y, C_tilde, X), self.log_q(Y, X, C, C_tilde)),
-                    tf.add(self.log_pi(Y, C, X), self.log_q(Y, X, C_tilde, C))
-                )
-            )
-        )
+    # @tf.function
+    # def metropolis_hastings(self, Y, X, C_tilde, C):
+    #     # print(self.log_pi(Y, C_tilde, X))
+    #     # print(self.log_q(Y, X, C, C_tilde))
+    #     # print(self.log_pi(Y, C, X))
+    #     # print(self.log_q(Y, X, C_tilde, C))
+    #     return tf.minimum(
+    #         self.one,
+    #         tf.math.exp(
+    #             tf.subtract(
+    #                 tf.add(self.log_pi(Y, C_tilde, X), self.log_q(Y, X, C, C_tilde)),
+    #                 tf.add(self.log_pi(Y, C, X), self.log_q(Y, X, C_tilde, C))
+    #             )
+    #         )
+    #     )
 
     @tf.function
     def __call__(self, Yk, start_index):
@@ -85,29 +88,34 @@ class BernoulliDF(tf.Module):
         Xk = self.X[:, start_index:end_index]
 
         # Update X
-        Xk = tf.add(
-            Xk,
-            tf.multiply(
-                self.gamma_X,
-                tf.matmul(self.C, self.gradient(Yk, self.C, Xk), transpose_a=True)
+        for _ in range(10):
+            Xk = tf.add(
+                Xk,
+                tf.multiply(
+                    self.gamma_X,
+                    tf.subtract(
+                        tf.matmul(self.C, self.gradient(Yk, self.C, Xk), transpose_a=True),
+                        tf.divide(Xk, self.sigma_X)
+                    )
+                )
             )
-        )
         self.X[:, start_index:end_index].assign(Xk)
 
         # Update C
         C_tilde = tf.add(
             self.C,
-            # tf.add(
-            #     self.gradient_log_pi(Yk, self.C, Xk),
-            #     tf.multiply(
-            #         tf.sqrt(2.0 * self.gamma_C),
-            #         tf.random.normal(tf.shape(self.C), mean=0.0, stddev=1.0, dtype=tf.float64)
-            #     )
-            # )
-            self.gradient_log_pi(Yk, self.C, Xk)
+            tf.add(
+                self.gradient_log_pi(Yk, self.C, Xk),
+                tf.multiply(
+                    tf.sqrt(2.0 * self.gamma_C),
+                    tf.random.normal(tf.shape(self.C), mean=0.0, stddev=1.0, dtype=tf.float64)
+                )
+            )
+            # self.gradient_log_pi(Yk, self.C, Xk)
         )
         
         # alpha = self.metropolis_hastings(Yk, Xk, C_tilde, self.C)
+        # # print(alpha)
         # if tf.random.uniform([], dtype=tf.float64) < alpha:
         #     self.C.assign(C_tilde)
         self.C.assign(C_tilde)
